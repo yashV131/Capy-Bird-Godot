1eaf1a4b8d31ebb50da9db182034d1651d1e1af1
yashV <147964547+yashV131@users.noreply.github.com>
2023-10-29 17:02:07 -0500

    Add files via upload

---
+++ Cappy Bird/Scenes/ground.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+
+signal hit
+
+#func _on_body_entered(body):
+#	hit.emit() # Replace with function body.
+
+
+func _on_body_entered(body):
+	hit.emit() # Replace with function body.
